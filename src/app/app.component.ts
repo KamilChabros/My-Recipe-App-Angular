1d957123a86932b9ff9ada47f8bb44d74fc66519
KamilChabros <kamilchabros@wp.pl>
2022-01-27 20:59:47 +0100

    Some functions added to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe';
+import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'myrecipeapp';
+  public recipes: Recipe[] = [];
+
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit(): void{
+    this.getRecipes();      
+  }
+
+  public getRecipes(): void {
+    this.recipeService.getRecipes().subscribe(
+      (response: Recipe[]) => {
+        this.recipes = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
 }
---
+++ src/app/recipe.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Recipe } from "./recipe";
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class RecipeService {
+    private apiServerUrl = '';
+
+    constructor(private http: HttpClient) {}
+
+    public getRecipes(): Observable<Recipe[]> {
+        return this.http.get<Recipe[]>(`${this.apiServerUrl}/recipe/all`)
+    }
+
+    public addRecipe(recipe: Recipe): Observable<Recipe> {
+        return this.http.post<Recipe>(`${this.apiServerUrl}/recipe/add`, recipe)
+    }
+
+    public updateRecipe(recipe: Recipe): Observable<Recipe> {
+        return this.http.put<Recipe>(`${this.apiServerUrl}/recipe/update`, recipe)
+    }
+
+    public deleteRecipe(recipeId: number): Observable<void> {
+        return this.http.delete<void>(`${this.apiServerUrl}/recipe/delete/${recipeId}`)
+    }
+}
