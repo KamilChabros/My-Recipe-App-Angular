e9f6480031971297532f338c09bd2d58e7621103
KamilChabros <kamilchabros@wp.pl>
2022-03-02 22:43:58 +0100

    app-routing.module.ts coded; app-component.ts coded;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminBoardComponent } from './admin-board/admin-board.component';
+import { CreatorBoardComponent } from './creator-board/creator-board.component';
+import { EditorBoardComponent } from './editor-board/editor-board.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
+import { UserBoardComponent } from './user-board/user-board.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'user', component: UserBoardComponent},
+  {path: 'admin', component: AdminBoardComponent},
+  {path: 'creator', component: CreatorBoardComponent},
+  {path: 'editor', component: EditorBoardComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
