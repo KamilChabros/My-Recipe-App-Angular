2d393dd80b945bc7c5993031f37d756fc13bcda2
KamilChabros <kamilchabros@wp.pl>
2022-03-05 16:01:29 +0100

    work in progress

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { LoginComponent } from '../login/login.component';
 
 const AUTH_API = 'http://localhost:8081/api/auth/';
 const httpOptions = {
