dce99d2e1912ce72ac783638feabd4048b2a7c42
KamilChabros <kamilchabros@wp.pl>
2022-05-16 21:42:51 +0200

    Hardcoded opinions in recipe-opinions.component

---
+++ src/app/model/Opinions.ts
@@ -0,0 +1,9 @@
+
+
+export class Opinion {
+    id: number;
+    user: string; // ????
+    rating: string;
+    content: string;
+    date: string;
+}
--- src/app/recipe-opinions/recipe-opinions.component.ts
+++ src/app/recipe-opinions/recipe-opinions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Opinion } from '../model/Opinions';
 
 @Component({
   selector: 'app-recipe-opinions',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-opinions.component.css']
 })
 export class RecipeOpinionsComponent implements OnInit {
+
+opinions: Opinion[]= [
+  {id: 1, user: 'Jan Kowalski', rating: 'OK', content:'Najlepsze szpagetti jakie jadłem', date: '2022-05-16'},
+  {id: 2, user: 'Danuta Nowak', rating: 'OK', content:'Klasyka w najlepszym wydaniu', date: '2022-05-16'},
+  {id: 3, user: 'Klaudiusz Adamski', rating: 'NOK', content:'Takie średnie bym powiedział', date: '2022-05-16'},
+  {id: 4, user: 'Zbigniew Wrotny', rating: 'OK', content:'Polecam, bardzo smaczne ;)', date: '2022-05-16'},
+]
 
   constructor() { }
 
