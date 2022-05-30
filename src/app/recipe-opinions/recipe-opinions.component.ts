6776c1c10c1e976ffbd778ef8102bd1bb34dc650
KamilChabros <kamilchabros@wp.pl>
2022-05-30 21:03:45 +0200

    printing opinions from backend at homepage

--- src/app/recipe-opinions/recipe-opinions.component.ts
+++ src/app/recipe-opinions/recipe-opinions.component.ts
@@ -9,13 +9,6 @@ import { RecipeOpinionsService } from './recipe-opinions.service';
 })
 export class RecipeOpinionsComponent implements OnInit {
 
-// opinions: Opinion[]= [
-//   {id: 1, username: 'Jan Kowalski', rating: 'OK', content:'Najlepsze szpagetti jakie jadłem', date: '2022-05-16'},
-//   {id: 2, username: 'Danuta Nowak', rating: 'OK', content:'Klasyka w najlepszym wydaniu', date: '2022-05-16'},
-//   {id: 3, username: 'Klaudiusz Adamski', rating: 'NOK', content:'Takie średnie bym powiedział', date: '2022-05-16'},
-//   {id: 4, username: 'Zbigniew Wrotny', rating: 'OK', content:'Polecam, bardzo smaczne ;)', date: '2022-05-16'}
-// ]
-
 opinions: Opinion[]= [];
   constructor(private recipeOpinionsService: RecipeOpinionsService) { }
 
